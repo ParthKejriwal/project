5d0b84247350aafed0f796939072802f6bca3743
ParthKejriwal <62254837+ParthKejriwal@users.noreply.github.com>
2020-03-23 12:32:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,54 @@
-var r = 0;
-var g = 50;
-var b=255;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+var engine, world;
 
-// INITIALIZE ALL YOUR VARIABLES
-// Ideally you would create 3 variables. red blue green or r, g, b
+var ground;
 
-
-function setup(){
-
-  createCanvas(1200,400);
+var stick1,stick2,stick3,stick4,stick5,stick6,stick7,stick8,stick9;
+function setup() {
+  var canvas=createCanvas(400,400);
+  engine = Engine.create();
+    world = engine.world;
+  stick1=new Sticks(20,360,20,100);
+  stick2=new Sticks(380,360,20,100);
+  stick3=new Sticks(60,300,100,20);
+  stick4=new Sticks(340,300,100,20);
+  stick5=new Sticks(100,240,20,100);
+  stick6=new Sticks(300,240,20,100);
+  stick7=new Sticks(140,180,130,20);
+  stick8=new Sticks(260,180,130,20);
+  stick9=new Sticks(200,130,30,80);
+ // ground=new Ground(200,395,400,20);
+ //ground=Bodies.rectangle(200,395,400,10);
+ //World.add(world,ground);
 }
 
-
-function draw(){
-  background(0);
-  let r = map(mouseX, 0, background, 0, 175);
-  // Scale the mouseX value from 0 to 640 to a range between 40 and 300
-  let g = map(mouseX, 10, background, 40, 300);
-  let b =map(mouseX,20,background,20,220);
-  fill(225,r,0);
- 
- // ellipse(width/2, height/2, r, r,r);
-  // change the value of Red based on the mouse movement about the X axis
-  // change the value of Green based on the mouse movement about the X axis
-  // change the value of Blue based on the mouse movement about the X axis
-
-  // Use the map() function to do so. 
-
-  // Pass the values to the background() function you have learnt previously.
-
-  // Create an ellipse that will move around with you mouse about the X axis.
-  // Remember to fill the canvas with white paint before creating the ellipse.
+function draw() {
+  background(0);  
+  Engine.update(engine);
+ // rectMode(CENTER);
+ // rect(ground.position.x,ground.position.y,400,10);
+// ground.display();
+  stick1.display();
+  stick2.display();
+  stick3.display();
+  stick4.display();
+  stick5.display();
+  stick6.display();
+  stick7.display();
+  stick8.display();
+  stick9.display();
+  circle(200,67,50);
+  square(110,147,45);
+  square(288,147,45);
+  rect(110,114,20,20);
+  rect(288,114,20,20);
+  circle(200,395,150);
+  square(200,230,40);
+  rect(55,215,15,150);
+  rect(350,215,15,150);
+  rect(55,130,30,20);
+  rect(350,130,30,20);
+  drawSprites();
 }
